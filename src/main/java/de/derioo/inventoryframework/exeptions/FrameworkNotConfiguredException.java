10f173f737d96767d582df2731569aa179cd652b
derio <derio@derioo.de>
2023-09-15 19:24:03 +0200

    fix(FrameworkNotConfiguredException): changed javadoc to the right text

--- src/main/java/de/derioo/inventoryframework/exeptions/FrameworkNotConfiguredException.java
+++ src/main/java/de/derioo/inventoryframework/exeptions/FrameworkNotConfiguredException.java
@@ -7,7 +7,7 @@ package de.derioo.inventoryframework.exeptions;
 public class FrameworkNotConfiguredException extends RuntimeException {
 
     /**
-     * Used to create an exeption with string
+     * Used to create an exception with string
      * @param s the name
      */
     public FrameworkNotConfiguredException(String s) {
